8719778561713968d9f97a4ee4c794958ddd7da0
MohamedSalman2371999 <95053768+MohamedSalman2371999@users.noreply.github.com>
2024-05-30 00:37:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -301,12 +301,3 @@ function validationInput(elemnt) {
 function closeModal() {
   modal.classList.add("d-none");
 }
-
-/* 
-===========>header animation
-*/
-TweenMax.to(".effect3d", 1.5, {
-  textShadow:
-    "0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0, 0, 0, .1), 0 0 5px rgba(0, 0, 0, .1), 0 1px 3px rgba(0, 0, 0, .3), 0 3px 5px rgba(0, 0, 0, .2), 0 5px 10px rgba(0, 0, 0, .25), 0 10px 10px rgba(0, 0, 0, .2), 0 20px 20px rgba(0, 0, 0, .15)",
-  ease: Sine.easeInOut,
-});
